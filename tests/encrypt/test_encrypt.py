478253acf343b4dd3721a089ebb94863743a3b77
root <root@DESKTOP-DFB6OFB>
2023-09-20 17:54:14 -0300

    feat: task 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
+    with pytest.raises(TypeError):
+        encrypt_message("1", "b")
+
+    assert encrypt_message("roquew", 3) == "qor_weu"
+
+    assert encrypt_message("roquew", 33) == "weuqor"
+
+    assert encrypt_message("roquew", 4) == "we_uqor"
     pass
