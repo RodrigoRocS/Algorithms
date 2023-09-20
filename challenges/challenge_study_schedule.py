4229604a3752f8769bb5e7471698e193f16d1008
root <root@DESKTOP-DFB6OFB>
2023-09-20 17:01:01 -0300

    feat: task 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    if target_time is None:
+        return None
+
+    if any(
+        not all(isinstance(value, int) for value in period)
+        for period in permanence_period
+    ):
+        return None
+
+    count = sum(
+        1 for x in permanence_period if x[0] <= target_time <= x[1]
+    )
+
+    return count
     raise NotImplementedError
